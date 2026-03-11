c1b962bbc37dc86a5d2235876aef655b5359e1fe
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-11 09:21:15 +0700

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,1 +1,12 @@
+pragma solidity ^0.8.20;
 
+contract Voting {
+
+    mapping(string => uint256) public votes;
+
+    function vote(string memory option) public {
+
+        votes[option]++;
+
+    }
+}
